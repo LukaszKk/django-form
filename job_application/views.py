53fe556bf38c2f880eca192b6450b6758b7e3a6d
Łukasz Kuk <lukasz010420@gmail.com>
2023-06-13 23:31:27 +0200

    form handling

--- job_application/views.py
+++ job_application/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ApplicationForm
+from datetime import datetime
+from .models import Form
+
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        form = ApplicationForm()
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = datetime.strptime(form.cleaned_data["date"], "%Y-%m-%d")
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date)
+            messages.success(request, "Form submitted successfully")
+    return render(request, "index.html")
